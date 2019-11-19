e372597c4b275bf4509f8f40187a563bc71e3766
Cristina Sirbu <sirbucristina.is@gmail.com>
2019-11-19 22:46:39 +0200

    Lazy loading of singleton

--- src/com/cristina/DBSingleton.java
+++ src/com/cristina/DBSingleton.java
@@ -1,9 +1,14 @@
 package com.cristina;
 
 public class DBSingleton {
-    private static DBSingleton instance = new DBSingleton();
+    private static DBSingleton instance = null;
     private DBSingleton() {}
     public static DBSingleton getInstance() {
+        // Lazy loading of the singleton
+        // NOTE: NOT thread safe
+        if (instance == null) {
+            instance = new DBSingleton();
+        }
         return instance;
     }
 }
