690c4f4a4091ca10a7cf0158c6927280ff08294c
Cristina Sirbu <sirbucristina.is@gmail.com>
2019-11-19 22:54:04 +0200

    Make the singleton thread safe

--- src/com/cristina/DBSingleton.java
+++ src/com/cristina/DBSingleton.java
@@ -1,13 +1,21 @@
 package com.cristina;
 
 public class DBSingleton {
-    private static DBSingleton instance = null;
-    private DBSingleton() {}
+    private static volatile DBSingleton instance = null;
+    private DBSingleton() {
+        // This should stop people from using reflection to instantiate this class
+        if (instance != null) {
+            throw new RuntimeException("Use getInstance() method to create");
+        }
+    }
     public static DBSingleton getInstance() {
-        // Lazy loading of the singleton
-        // NOTE: NOT thread safe
         if (instance == null) {
-            instance = new DBSingleton();
+            // This should check if other threads has created the instance already or not
+            synchronized (DBSingleton.class) {
+                if (instance == null) {
+                    instance = new DBSingleton();
+                }
+            }
         }
         return instance;
     }
