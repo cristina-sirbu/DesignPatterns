a9e573d69c81f69af21770a92bbdfc251a6debdd
Cristina Sirbu <sirbucristina.is@gmail.com>
2019-11-25 23:04:09 +0200

    Add true Builder implementation

--- src/com/cristina/builder/LunchOrderDemo.java
+++ src/com/cristina/builder/LunchOrderDemo.java
@@ -2,14 +2,22 @@ package com.cristina.builder;
 
 public class LunchOrderDemo {
     public static void main(String[] args) {
-//        LunchOrderBean lunchOrderBean = new LunchOrderBean();
+//      1. Builder using Beans
 
+//        LunchOrderBean lunchOrderBean = new LunchOrderBean();
 //        lunchOrderBean.setBread("Wheat");
 //        lunchOrderBean.setCondiments("Lettuce");
 //        lunchOrderBean.setDressing("Mustard");
 //        lunchOrderBean.setMeat("Ham");
 
-        LunchOrderTele lunchOrder = new LunchOrderTele("Wheat", "Lettuce", "Mustard","Ham");
+//      2. Builder using telescopic constructors
+//        LunchOrderTele lunchOrder = new LunchOrderTele("Wheat", "Lettuce", "Mustard","Ham");
+
+//      3. True Builder Pattern
+        LunchOrder.Builder builder = new LunchOrder.Builder();
+        builder.bread("Wheat").condiments("Lettuce").dressing("Mustard").meat("Ham");
+
+        LunchOrder lunchOrder = builder.build();
 
         System.out.println(lunchOrder.getBread());
         System.out.println(lunchOrder.getCondiments());
